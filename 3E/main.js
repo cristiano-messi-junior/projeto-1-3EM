dde1aa9bbef042e445a63eec2d4801d4fcdcca42
cristiano-messi-junior <161607729+cristiano-messi-junior@users.noreply.github.com>
2024-03-27 11:13:02 -0300

    main.js

--- 3E/main.js
+++ 3E/main.js
@@ -1,1 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
+
+fort(let i=0;1 <botoes.lenght;i++){
+  botoes[i].onclick = function(){
+
+    for(let j=0;j<botoes.lenght;j++){
+      botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+  }
+}
